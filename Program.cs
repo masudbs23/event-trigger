e5f396b117573ba519308d0f56b02b7a807eb2d4
Md. Masud <masud@bs-23.net>
2019-04-05 15:49:49 +0600

    initial message

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace event_trigger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var trigger = new DailyTrigger(15, 42); // every day at 4:00pm
+
+            trigger.OnTimeTriggered += () =>
+            {
+                Console.WriteLine("Funciton is triggered at :" + DateTime.Now);
+            };
+
+            Console.ReadKey();
+        }
+    }
+}
